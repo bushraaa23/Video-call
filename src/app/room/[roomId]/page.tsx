beb590eb5973885918e567fe9deb257867727d02
Bushra Arshad <iqra37496@gmail.com>
2024-08-31 21:56:30 +0500

    Amazing Video Calling app

---
+++ src/app/room/[roomId]/page.tsx
@@ -0,0 +1,55 @@
+'use client';
+import react from "react"
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+
+export default function room ({params}:{ params: { roomId:string } } ){
+const roomID : string = params.roomId;
+
+
+
+const myMeeting = async (element:any) => {
+
+    // generate Kit Token
+     const appID : number = Number (process.env.NEXT_PUBLIC_AAID);
+     const serverSecret : string = process.env.NEXT_PUBLIC_SERVER_SECRETE as string;
+     const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(
+        appID,
+        serverSecret, 
+        roomID, 
+        Date.now().toString(),
+
+        "Enter your name here,"
+     );
+   
+    // Create instance object from Kit Token.
+     const zp = ZegoUIKitPrebuilt.create(kitToken);
+     // start the call
+     zp.joinRoom({
+       container: element,
+       sharedLinks: [
+         {
+           name: 'Personal link',
+           url:
+            window.location.protocol + '//' + 
+            window.location.host + window.location.pathname +
+             '?roomID=' +
+             roomID,
+         },
+       ],
+       scenario: {
+         mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+       },                          //GroupCall,
+     });                           //  ^
+                                    // i
+    };
+
+
+    return
+        <div 
+       ref={myMeeting}
+       className="w-[100vw] h-[100vh]">
+             
+
+        </div>;
+    
+}
